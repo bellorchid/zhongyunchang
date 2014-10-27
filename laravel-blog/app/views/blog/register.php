d487914b2de48ac24947adb8cdcee47a26082120
bellorchid <zhongyunchang@yeah.net>
2014-10-27 23:34:03 +0800

    增加登录功能

---
+++ laravel-blog/app/views/blog/register.php
@@ -0,0 +1,10 @@
+﻿
+<form action="../models/blog/regcheck.php" method="post" charset="UTF-8">
+    用户名：<input type="text" name="username"/>
+    <br/>
+    密　码:<input type="password" name="password"/>
+    <br/>
+    确认密码：<input type="password" name="confirm"/>
+    <br/>
+    <input type="Submit" name="Submit" value="注册"/>
+</form>
---
+++ laravel-blog/index.php
@@ -0,0 +1,3 @@
+<form>
+    <a href="./app/views/blog/login.php">注册</a>
+</form>
